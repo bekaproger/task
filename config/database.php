934a5d2c8ffd1d60a83157180a2378e7181b05c4
Bekzod <bekanakk@gmail.com>
2020-01-07 02:08:06 +0500

    added htaccess

--- config/database.php
+++ config/database.php
@@ -8,5 +8,12 @@ return [
             'driver' => 'pdo_sqlite',
             'path' => dirname(__DIR__).'/db.sqlite',
         ],
+        'pdo_mysql' => [
+            'dbname' => env('DB_NAME'),
+            'user' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
+            'host' => env('DB_HOST'),
+            'driver' => 'pdo_mysql'
+        ]
     ],
 ];
