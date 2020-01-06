2a3afabb33f1727620a492f4a922500edb2e30d4
Bekzod <bekanakk@gmail.com>
2020-01-07 02:22:08 +0500

    added htaccess

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -62,7 +62,7 @@ class AuthController extends AbstractController
         $user->setName($name);
         $user->setEmail($email);
         $user->setPassword(password_hash($password, PASSWORD_BCRYPT));
-        $user->setIsAdmin(false);
+        $user->setIsAdmin(true);
         $this->getManager()->persist($user);
 
         $this->getManager()->flush();
